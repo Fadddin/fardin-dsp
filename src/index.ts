84f0ec8ec94dcd9efad7f179623daad8e5b3a1ed
Fadddin <fardinkhan479@gmail.com>
2024-11-27 20:02:58 +0530

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express, { Request, Response } from "express";
 import path from "path";
+import cors from "cors";
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 
 app.get("/", (req: Request, res: Response) => {
     res.json({
